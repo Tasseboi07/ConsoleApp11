891e1898af13ac4218e156cae65d9712d535342c
johe07003 <johe07003@E-PW05C37X>
2024-11-14 13:12:30 +0100

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace hej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din ålder");
+            int ålder= int.Parse(Console.ReadLine());
+            if (ålder <= 16 )
+            {
+                Console.WriteLine("Du är för ung, väx upp för fan");
+            }
+            else if( ålder >= 19 )
+            {
+                Console.WriteLine("Du är för gammal, clicka ok för att åka till ålderdomsehem");
+            }
+            else
+            {
+                Console.WriteLine("Välkommen hit gubbe");
+            }
+        }
+    }
+}
